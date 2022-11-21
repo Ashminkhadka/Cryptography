79a95f44514d480fccd893f9d619e2b3350f3f0e
Ashminkhadka <70682035+Ashminkhadka@users.noreply.github.com>
2022-11-21 17:32:48 +0545

    Update hillClimb.cpp

--- hillClimb.cpp
+++ hillClimb.cpp
@@ -1,40 +1,134 @@
-#include<iostream>
-#include<vector>
+//b)Program to implement the hill cipher.
+#include <iostream>
+#include <string>
 using namespace std;
-int main(){
-    int x,y,i,j,k,n;
-    cout<<"Encryption:";
-    cout<<"Enter the size of key matrix\n";
-    cin>>n;
-    cout<<"Enter the key matrix\n";
-    int a[n][n];
-    for(i=0;i<n;i++){
-        for(j=0;j<n;j++){
-            cin>>a[i][j];
-        }
-    }
-    cout<<"Enter your name:\n";
-    string s;
-    cin>>s;
-    int temp = (n-s.size()%n)%n;
-    for(i=0;i<temp;i++){
-        s+='x';
-    }
-    k=0;
-    string ans="";
-    while(k<s.size()){
-        for(i=0;i<n;i++){
-            int sum = 0;
-            int temp = k;
-            for(j=0;j<n;j++){
-                sum += (a[i][j]%26*(s[temp++]-'a')%26)%26;
-                sum = sum%26;
-            }
-            ans+=(sum+'a');
-        }
-        k+=n;
-    }
-    cout<<ans<<'\n';
+#define M 3
+int inverse(int b)
+{
+	int inv;
+	int q, r, r1 = 26, r2 = b, t, t1 = 0, t2 = 1;
 
-    return 0;
+	while (r2 > 0) {
+		q = r1 / r2;
+		r = r1 - q * r2;
+		r1 = r2;
+		r2 = r;
+
+		t = t1 - q * t2;
+		t1 = t2;
+		t2 = t;
+	}
+
+	if (r1 == 1) {
+		inv = t1;
+		if (inv < 0)
+			inv = inv + 26;
+		return inv;
+	}
+
+	return -1;
+}
+
+void inverseMatrix(int key[][3], int inv[][3])
+{
+	int C[3][3]; 
+	int A[3][3]; 
+	C[0][0] = key[1][1] * key[2][2] - key[2][1] * key[1][2];
+	C[0][1] = -(key[1][0] * key[2][2] - key[2][0] * key[1][2]);
+	C[0][2] = key[1][0] * key[2][1] - key[1][1] * key[2][0];
+	C[1][0] = -(key[0][1] * key[2][2] - key[2][1] * key[0][2]);
+	C[1][1] = key[0][0] * key[2][2] - key[2][0] * key[0][2];
+	C[1][2] = -(key[0][0] * key[2][1] - key[2][0] * key[0][1]);
+	C[2][0] = key[0][1] * key[1][2] - key[0][2] * key[1][1];
+	C[2][1] = -(key[0][0] * key[1][2] - key[1][0] * key[0][2]);
+	C[2][2] = key[0][0] * key[1][1] - key[1][0] * key[0][1];
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			A[i][j] = C[j][i];
+		}
+	}
+	int det = key[0][0] * C[0][0] + key[0][1] * C[0][1] + key[0][2] * C[0][2];
+
+	if (det == 0 || det % 2 == 0 || det % 13 == 0) {
+		printf("The text cannot be encrypted. Take valid key.\n");
+		exit(1);
+	}
+	int invs = inverse(det);
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			inv[i][j] = A[i][j] * invs;
+		}
+	}
+	printf("\nInverse of a Key-\n\n");
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			if (inv[i][j] >= 0) {
+				inv[i][j] = inv[i][j] % 26;
+				printf("%3d", inv[i][j]);
+			}
+			else {
+				for (int x = 0;; x++) {
+					if (x * 26 + inv[i][j] > 0) {
+						inv[i][j] = x * 26 + inv[i][j];
+						break;
+					}
+				}
+				printf("%3d", inv[i][j]);
+			}
+		}
+		printf("\n");
+	}
+}
+string encrypt(string text, int key[][3])
+{
+	string c = "";
+	int k = 0;
+	int input[3];
+	while (k < text.length()) {
+		input[0] = text[k++] - 65;
+		input[1] = text[k++] - 65;
+		input[2] = text[k++] - 65;
+		for (int i = 0; i < M; i++) {
+			int cipher = 0;
+			for (int j = 0; j < M; j++) {
+				cipher += key[i][j] * input[j];
+			}
+			c += (cipher % 26) + 65;
+		}
+	}
+	return c;
+}
+string decrypt(string s, int inv[][3])
+{
+	string d = "";
+	int k = 0;
+	int input[3];
+	while (k < s.length()) {
+		input[0] = s[k++] - 65;
+		input[1] = s[k++] - 65;
+		input[2] = s[k++] - 65;
+		for (int i = 0; i < M; i++) {
+			int decipher = 0;
+			for (int j = 0; j < M; j++) {
+				decipher += inv[i][j] * input[j];
+			}
+			d += (decipher % 26) + 65;
+		}
+	}
+	return d;
+}
+int main()
+{
+	int key[3][3] = {
+		{ 4, 24, 1 },
+		{ 1, 16, 10 },
+		{ 25, 17, 15 }
+	};
+	string text = "ASHMINKHADKA";
+	string c = encrypt(text, key);
+	cout << "Corresponding cipher text is - " << c;
+	int inv[3][3];
+	inverseMatrix(key, inv);
+	cout << "\nCorresponding decrypted text is - " << decrypt(c, inv);
+	return 0;
 }
